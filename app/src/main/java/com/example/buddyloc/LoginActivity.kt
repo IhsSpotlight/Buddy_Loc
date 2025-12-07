925a9d34b1276e13dc42c3ab5e632d99cf34111a
IMAM H SHABBIR <85285483+IhsSpotlight@users.noreply.github.com>
2025-12-08 00:55:44 +0600

    Login Using Firestore Complete

--- app/src/main/java/com/example/buddyloc/LoginActivity.kt
+++ app/src/main/java/com/example/buddyloc/LoginActivity.kt
@@ -1,20 +1,62 @@
 package com.example.buddyloc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var ViewModel: AuthenticationViewModel
+    private lateinit var btnlogin: Button
+    private lateinit var btngotoregister: Button
+    private lateinit var btngotoforgotpassword: Button
+    private lateinit var emailedittext: EditText
+    private lateinit var passwordedittext: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnlogin = findViewById(R.id.loginbtn)
+        btngotoregister = findViewById(R.id.createacc)
+        btngotoforgotpassword = findViewById(R.id.forgetpasswordbtn)
+        emailedittext = findViewById(R.id.emailet)
+        passwordedittext = findViewById(R.id.passet)
+
+
+        btnlogin.setOnClickListener {
+            val email = emailedittext.text.toString()
+            val password = passwordedittext.text.toString()
+
+            ViewModel.login(
+                email, password,
+                {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }, { errormessage ->
+                    Toast.makeText(this, errormessage, Toast.LENGTH_SHORT).show()
+
+                })
+        }
+
+        btngotoregister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
+    // Correct
+    override fun onStart() {
+        super.onStart()
+        if (Firebase.auth.currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
 }
